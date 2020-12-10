c934488104462275027f05d03ebe2fcdc70962aa
sanja <sanjayshrestha969@gmail.com>
2020-12-10 12:55:10 +0545

    Autocomplete and Spinner

---
+++ app/src/main/java/com/sanjay/kotlinapp/AutoComplete.kt
@@ -0,0 +1,11 @@
+package com.sanjay.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AutoComplete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auto_complete)
+    }
+}
