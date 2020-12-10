f3ec03fe6573d518ed168d75d8df4e9c9870722a
sanja <sanjayshrestha969@gmail.com>
2020-12-10 13:35:15 +0545

    Class

--- app/src/main/java/com/sanjay/kotlinapp/AutoComplete.kt
+++ app/src/main/java/com/sanjay/kotlinapp/AutoComplete.kt
@@ -2,10 +2,25 @@ package com.sanjay.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class AutoComplete : AppCompatActivity() {
+
+    private val languages = arrayOf("Nepali","English","Chinese","Hindi")
+    private lateinit var autoComplete: AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auto_complete)
+
+        autoComplete = findViewById(R.id.autocomplete)
+
+        val autoCompleteAdapter = ArrayAdapter(
+            applicationContext,android.R.layout.simple_dropdown_item_1line,languages
+        )
+        autoComplete.setAdapter(autoCompleteAdapter)
+        autoComplete.threshold = 1
+
     }
 }
--- app/src/main/java/com/sanjay/kotlinapp/SpinnerActivity.kt
+++ app/src/main/java/com/sanjay/kotlinapp/SpinnerActivity.kt
@@ -2,10 +2,44 @@ package com.sanjay.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class SpinnerActivity : AppCompatActivity() {
+
+    private val languages = arrayOf("  ","Nepali","Hindi","Chinese","English")
+    private lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
+
+        spinner = findViewById(R.id.spinner)
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,languages)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem =  parent?.getItemAtPosition(position).toString()
+                Toast.makeText(applicationContext, "Selected Item : $selectedItem", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+
+        }
+
     }
+
 }
