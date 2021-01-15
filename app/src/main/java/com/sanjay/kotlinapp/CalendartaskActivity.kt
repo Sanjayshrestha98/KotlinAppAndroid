96dff9e44614dc8bd86874a0b1d856e7a505d53b
sanja <sanjayshrestha969@gmail.com>
2021-01-15 18:08:27 +0545

    taskComplete

--- app/src/main/java/com/sanjay/kotlinapp/CalendartaskActivity.kt
+++ app/src/main/java/com/sanjay/kotlinapp/CalendartaskActivity.kt
@@ -29,13 +29,14 @@ private lateinit var tvDate : TextView
         etyourage = findViewById(R.id.etyourage)
         tvDate = findViewById(R.id.tvDate)
 
-        etjoindate.setOnClickListener {
-            loadCalenderforjoin()
-        }
-        etdob.setOnClickListener {
-            loadCalenderforage()
+        etjoindate.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus)loadCalenderforjoin()
+
         }
 
+
+        etdob.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus)loadCalenderforage()  }
     }
 
     private fun loadCalenderforjoin(){
@@ -44,12 +45,20 @@ private lateinit var tvDate : TextView
             this,
             DatePickerDialog.OnDateSetListener{
                     view,yearNew, monthOfYear, dayOfMonth ->
-                etjoindate.setText("Selected Date : $dayOfMonth/${monthOfYear+1}/$year")
-                etyearsworked.setText("${year - yearNew} Years, ${month - monthOfYear} months")
+                etjoindate.setText("Selected Date : $dayOfMonth/$monthOfYear/$yearNew")
+
+                if(month-monthOfYear < 0  ){
+                    var mth = (month - monthOfYear) + 12
+                    var yer = (year - yearNew) - 1
+                    etyearsworked.setText("You have worked for ${yer} year and ${mth} months")
+                }
+                else
+                etyearsworked.setText("You have worked for ${year - yearNew} years and ${month-monthOfYear} months")
             },
             year, month, day
         )
         datePickerDialog.show()
+        etjoindate.clearFocus()
     }
 
     private fun loadCalenderforage(){
@@ -57,12 +66,22 @@ private lateinit var tvDate : TextView
             this,
             DatePickerDialog.OnDateSetListener{
                     view,yearNew, monthOfYear, dayOfMonth ->
-                etdob.setText("Selected Date : $dayOfMonth/${monthOfYear+1}/$year")
-                etyourage.setText("${year - yearNew}")
+                etdob.setText("Selected Date : $dayOfMonth/${monthOfYear+1}/$yearNew")
+
+                if(month-monthOfYear < 0  ){
+                    var mth = (month - monthOfYear) + 12
+
+                    var yer = (year - yearNew) - 1
+                    etyourage.setText("You are $yer Years and $mth months old")
+                }
+                else
+                etyourage.setText("You are ${year - yearNew} Years and ${month-monthOfYear} months old")
+
             },
-            year-10, month, day
+            year, month, day
         )
         datePickerDialog.show()
+        etdob.clearFocus()
     }
 
 }
